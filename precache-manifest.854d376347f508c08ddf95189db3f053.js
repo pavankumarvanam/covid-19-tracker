3d250de004c0547b703987b185279397c1f18e83
pavankumarvanam <v.pavan@ailabs.in>
2020-07-11 14:01:19 +0530

    Updates

---
+++ precache-manifest.854d376347f508c08ddf95189db3f053.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6e571cefd7afc10d6d55315e90a29e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a526ec1b459c18791552",
+    "url": "/covid-19-tracker/static/css/main.c9c93494.chunk.css"
+  },
+  {
+    "revision": "2798ad20cf8e28205806",
+    "url": "/covid-19-tracker/static/js/2.bfe943a2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.bfe943a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a526ec1b459c18791552",
+    "url": "/covid-19-tracker/static/js/main.92c1b1a2.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "52abb9e20604da2ba596ee86d495ca97",
+    "url": "/covid-19-tracker/static/media/united-nations-covid-19-response-KSUv52EZgGg-unsplash-min.52abb9e2.jpg"
+  }
+]);
